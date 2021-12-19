adc19b7a79592d08e2caac700d73fd66252ad43b
Artur Bogusiak <artur.bogusiak@gmail.com>
2021-12-19 17:12:39 +0100

    Initial commit.

---
+++ src/main/java/com/expense/ExpenseApplication.java
@@ -0,0 +1,14 @@
+package com.expense;
+
+import com.expense.domain.Money;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseApplication.class, args);
+	}
+
+}
